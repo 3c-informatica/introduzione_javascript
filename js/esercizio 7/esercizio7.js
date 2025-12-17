1cb02fb9328dda6ea083c6eb230c6f9c6309d96c
unknown <e.andru.3864@vallauri.net>
2025-12-17 09:03:34 +0100

    finire l'esercizio 7

---
+++ js/esercizio 7/esercizio7.js
@@ -0,0 +1,7 @@
+const formelement = document.getElementById('form-completo');
+const inputUsername = document.getElementById('input-username');
+const selectCitta = document.getElementById('select-citta');
+const boxRisultato = document.getElementById('risultato');
+
+
+const radioUomo = document.getElementById('radio-uomo');
